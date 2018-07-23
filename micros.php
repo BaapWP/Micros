3d8b91d5c7bc6b5089c56c75fb95d9933338a84d
Jitender Singh <jitender.singhwp@gmail.com>
2018-07-23 06:39:25 +0530

    plugin header information updated

---
+++ micros.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name:  Micros
+Version:      0.0.1
+Author:       Jitender Singh
+Author URI:   https://github.com/jitendersinghwp/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  micros
+Domain Path:  /languages
+*/
