be46ac4b6adb661a4a4fcfba59a86f9c91e7a682
Jitender Singh <jitender.singhwp@gmail.com>
2018-07-25 08:29:48 +0530

    micro menu page added

--- micros.php
+++ micros.php
@@ -12,3 +12,31 @@ Domain Path:  /languages
 
 //directory constant
 define( 'Micros_PLUGIN_DIR', plugin_dir_path( __FILE ) );
+
+//register Micro page hook to admin menu
+add_action('admin_menu', 'micros_page_register');
+
+if(!function_exists('micros_page_register')){
+    function micros_page_register(){
+        add_menu_page(
+            __('Micros', 'micros'),
+            __('Micros', 'micros'),
+            'manage_options',
+            'micros',
+            'micro_page_render_callback',
+            'dashicons-editor-expand',
+            70
+        );
+    }
+}
+// add_menu_page callback
+if(!function_exists('micro_page_render_callback')){
+    function micro_page_render_callback(){
+        global $title;
+        ?>
+        <div class="wrap">
+            <h1><?php echo $title; ?></h1>
+        </div>
+        <?php
+    }
+}
