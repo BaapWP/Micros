bfbb092d2aec3bdcefb16a125e3950ee47682898
Unknown <jitender.singhwp@gmail.com>
2018-07-28 10:41:55 +0530

    code documented

--- micros.php
+++ micros.php
@@ -11,26 +11,32 @@
  */
 
 /**
- * Summary.
+ * creates plugin root directory path
  *
- * @var string The
+ * @var string
  */
 define( 'MICROS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
+// creates upload directory path when 'MICROS_UPLOADS_DIR' constant already not defined
 if ( ! defined( 'MICROS_UPLOAD_DIR' ) ) {
 	/**
-	 *
+	 * creates upload directory path where all micors store
+     *
+     * @var string
 	 */
 	define( 'MICROS_UPLOAD_DIR', WP_CONTENT_DIR . '/micros' );
 }
 
 /**
- *
+ * provide functions to create admin pages
  */
 include_once( MICROS_PLUGIN_DIR . 'admin/class-micros-admin.php' );
 
+// makes instance of 'MICRO_ADMIN' class
 $micros_admin = new Micros_Admin();
 
+// render admin pages
 $micros_admin->hook();
 
+// unset instance
 unset( $micros_admin );
