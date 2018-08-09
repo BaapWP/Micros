00b43dae3ac60b366af5c953b76e9d7a0af66133
Unknown <jitender.singhwp@gmail.com>
2018-08-09 22:40:29 +0530

    Revamp documentation

--- micros.php
+++ micros.php
@@ -21,16 +21,15 @@
  */
 
 /**
- * creates plugin root directory path
+ * Creates plugin root directory path
  *
  * @var string
  */
 define( 'MICROS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
-// creates upload directory path when 'MICROS_UPLOADS_DIR' constant already not defined
 if ( ! defined( 'MICROS_UPLOAD_DIR' ) ) {
 	/**
-	 * creates upload directory path where all micors store
+	 * Creates upload directory path where all micors store
      *
      * @var string
 	 */
@@ -38,15 +37,15 @@ if ( ! defined( 'MICROS_UPLOAD_DIR' ) ) {
 }
 
 /**
- * provide functions to create admin pages
+ * Provides functions to create admin pages
  */
 include_once( MICROS_PLUGIN_DIR . 'admin/class-micros-admin.php' );
 
-// makes instance of 'MICRO_ADMIN' class
+// Makes instance of 'MICRO_ADMIN' class
 $micros_admin = new Micros_Admin();
 
-// render admin pages
+// Render admin pages
 $micros_admin->hook();
 
-// unset instance
+// Unset instance
 unset( $micros_admin );
