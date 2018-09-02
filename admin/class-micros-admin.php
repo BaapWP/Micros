1f06dcf775896b7355e36eb4debd1a2055aa33e2
Unknown <jitender.singhwp@gmail.com>
2018-09-02 18:25:11 +0530

    Add file browser

--- admin/class-micros-admin.php
+++ admin/class-micros-admin.php
@@ -7,6 +7,11 @@
  * @since 0.0.1
  */
 
+/**
+ * Require helper functions
+ */
+require_once( MICROS_PLUGIN_DIR.'/admin/helpers.php' );
+
 //Exits if 'ABSPATH' constant not defined
 defined( 'ABSPATH' ) || exit();
 
@@ -92,10 +97,30 @@ if ( ! class_exists( 'Micros_Admin' ) ) {
 
 			// Adds inline script to set plugin name in  'wp.themePluginEditor.themeOrPlugin'
 			wp_add_inline_script( 'wp-theme-plugin-editor', 'wp.themePluginEditor.themeOrPlugin = "micros";' );
+
+            // List of allowable extensions
+            $editable_extensions = wp_get_plugin_file_editable_extensions( $micro );
+
+            $micro_files = get_micro_files();
+            //error_log( print_r( $micros, true ), 3, WP_CONTENT_DIR.'/debug.log' );
 			?>
             <div class="wrap">
                 <div id="templateside">
                     <h2 id="micro-files-label"><?php _e( 'Micro Files' ); ?></h2>
+                    <?php
+                    $micro_editable_files = array();
+                    foreach ( $micro_files as $micro_file ) {
+                        if ( preg_match('/\.([^.]+)$/', $micro_file, $matches ) && in_array( $matches[1], $editable_extensions ) ) {
+                            $micro_editable_files[] = $micro_file;
+                        }
+                    }
+                    ?>
+                    <ul role="tree" aria-labelledby="plugin-files-label">
+                        <li role="treeitem" tabindex="-1" aria-expanded="true" aria-level="1" aria-posinset="1" aria-setsize="1">
+                            <ul role="group">
+                                <?php wp_print_micro_file_tree( wp_make_micro_file_tree( $micro_editable_files ) ); ?>
+                            </ul>
+                    </ul>
                 </div>
                 <form action="#" id="sub-template">
                     <div>
