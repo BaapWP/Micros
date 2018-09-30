59d0fc11b5d42c5f81850b7fb5e6a4fc0bfe1a68
Unknown <jitender.singhwp@gmail.com>
2018-10-01 00:26:22 +0530

    Add get_micro function

--- admin/helpers.php
+++ admin/helpers.php
@@ -34,7 +34,7 @@ function get_micro_files( ) {
  */
 function wp_make_micro_file_tree( $micro_editable_files ) {
     $tree_list = array();
-    error_log( print_r( $micro_editable_files, true ), 3, WP_CONTENT_DIR.'/debug.log' );
+    //error_log( print_r( $micro_editable_files, true ), 3, WP_CONTENT_DIR.'/debug.log' );
     foreach ( $micro_editable_files as $micro_file ) {
         $list     = explode( '/', preg_replace( '#^.+?/#', '', $micro_file ) );
         $last_dir = &$tree_list;
@@ -111,3 +111,81 @@ function wp_print_micro_file_tree( $tree, $label = '', $level = 2, $size = 1, $i
         <?php
     }
 }
+
+if( !function_exists('get_micros') ):
+    /**
+     * Check the micros directory and retrieve all micro files with all meta data.
+     *
+     * The file with the micro meta data is the file that will be included and therefore
+     * needs to have the main execution for the micro. This does not mean
+     * everything must be contained in the file and it is recommended that the file
+     * be split for maintainability. Keep everything in one file for extreme
+     * optimization purposes.
+     *
+     * @since 1.5.0
+     *
+     * @param string $micro_folder Optional. Relative path to single micro folder.
+     * @return array Key is the plugin file path and the value is an array of the plugin data.
+     */
+    function get_micros($micros_folder = '') {
+
+        if ( ! $cache_micros = wp_cache_get('micros', 'micros') )
+            $cache_micros = array();
+
+        if ( isset($cache_micros[ $micros_folder ]) )
+            return $cache_micros[ $micros_folder ];
+
+        $wp_micros = array ();
+        $micro_root = MICROS_UPLOAD_DIR;
+        if ( !empty($micros_folder) )
+            $micro_root .= $micros_folder;
+
+        // Files in wp-content/micros directory
+        $micros_dir = @ opendir( $micro_root);
+
+        $micro_files = array();
+        if ( $micros_dir ) {
+            while (($file = readdir( $micros_dir ) ) !== false ) {
+                if ( substr($file, 0, 1) == '.' )
+                    continue;
+                if ( is_dir( $micro_root.'/'.$file ) ) {
+                    $micros_subdir = @ opendir( $micro_root.'/'.$file );
+                    if ( $micros_subdir ) {
+                        while (($subfile = readdir( $micros_subdir ) ) !== false ) {
+                            if ( substr($subfile, 0, 1) == '.' )
+                                continue;
+                            if ( $subfile == 'micros.json' )
+                                $micro_files[] = "$micro_root/$file/$subfile";
+                        }
+                        closedir( $micros_subdir );
+                    }
+                } else {
+                    if ( $file == 'micros.json' )
+                        $micro_files[] = $file;
+                }
+            }
+            closedir( $micros_dir );
+        }
+
+        if ( empty($micro_files) )
+            return $wp_micros;
+
+        foreach ( $micro_files as $micro_file ) {
+
+            if ( !is_readable( "$micro_file" ) )
+                continue;
+
+            $get_micro_data = file_get_contents( $micro_file );
+            $decode_micro_data = json_decode( preg_replace( '/,(?!\s*?[\{\[\"\'\w])/i', '', $get_micro_data ), true );
+
+            if ( empty ( $decode_micro_data['name'] ) )
+                continue;
+
+            $wp_micros[ $micro_file ] = $decode_micro_data;
+        }
+
+        $cache_micros[ $micros_folder ] = $wp_micros;
+        wp_cache_set('micros', $cache_micros, 'micros');
+        return $wp_micros;
+    }
+endif;
